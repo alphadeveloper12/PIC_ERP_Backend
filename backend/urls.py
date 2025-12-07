2cc1ecd6e43693614430b607fe3d7891195fdd73
mughal41 <mughalusama43@gmail.com>
2025-12-07 23:36:43 +0500

    Onboarding Phase

--- backend/urls.py
+++ backend/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    # path('api/auth/', include('auth_app.urls')),  # our login API
+    path('api/auth/', include('auth_app.urls')),  # our login API
     # path('api/', include('chatgpt.urls')),  # OpenAI API endpoints
     # path('api/excel/', include('excel.urls')),  # Excel/Bill of Quantities endpoints
     # path('api/excel/', include('excel.urls')),  # Excel/Bill of Quantities endpoints
